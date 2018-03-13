2cc2c0ee9f01ef2effb254b8ca14f03e26f54837
Ian Carthey <33433194+iancarthey@users.noreply.github.com>
2018-03-12 23:26:12 -0500

    completed challenge 1

--- client.js
+++ client.js
@@ -1,31 +1,40 @@
 console.log('js');
 
 $(document).ready(readyNow);
+//global variable to use for click counter
 let clickCount = 0;
 
 function readyNow(){
+  //checking to see if jquery linked
   console.log('jquery sourced');
+  //generate click function
   $('#generateButton').on('click', addDiv);
 }
 
 function addDiv(){
   console.log('in add div');
   clickCount++;
-  $('body').append('<div id="divNumba1"></div>');
-  $('#divNumba1').append('<p class="background">Number of Clicks: ' + clickCount + '</p>');
-  $('#divNumba1').append('<button class="swapButton">Swap</button><button class="deleteButton">Delete</button>');
-  // $('.background').css('background-color', 'red');
-  $('.swapButton').on('click', function(){
-  });
-  $('.deleteButton').on('click', deleteDiv);
+  // create div for step 2
+  $('#container').append('<div id="divNumba1"></div>');
+  // create p within divNumba2 for step 3
+  $('#divNumba1').append('<div class="divNumba2"><p>Number of Clicks: ' + clickCount + '</p></div>');
+  // add switch button
+  $('.switchButton').remove();
+  $('.divNumba2').append('<button class="switchButton">switch</button>');
+  // switch button event listener
+  $('#divNumba1').on('click', '.switchButton' , switchColor);
+  // add delete button
+  $('.deleteButton').remove();
+  $('.divNumba2').append('<button class="deleteButton">delete</delete>');
+  // delete button event listener
+  $('#divNumba1').on('click', '.deleteButton', deleteDiv);
+}
+// switch color function to toggle yellow and red
+function switchColor(){
+  $(this).parent().toggleClass('highlight');
 }
 
-// function switchColor(){
-//   $(this).prev().css('background-color', 'yellow');
-// }
-
+//function to delete parent div
 function deleteDiv(){
-  $(this).prev().remove();
-  $(this).prev().remove();
-  $(this).remove();
+  $(this).parent().remove();
 }
